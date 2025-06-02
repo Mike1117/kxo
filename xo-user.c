98525c3e3c829deda3e9ebe36b75cfe612d4a188
MikeChen <mike18913235447@gmail.com>
2025-06-02 15:04:55 +0800

    Move draw_board() function to user space
    Removed draw_board() and draw_buffer from kernel module (main.c).
    Now board will be drawed in user space.

--- xo-user.c
+++ xo-user.c
@@ -17,6 +17,8 @@
 #define XO_DEVICE_FILE "/dev/kxo"
 #define XO_DEVICE_ATTR_FILE "/sys/class/kxo/kxo/kxo_state"
 
+static char draw_buffer[DRAWBUFFER_SIZE];
+
 static bool status_check(void)
 {
     FILE *fp = fopen(XO_STATUS_FILE, "r");
@@ -85,58 +87,6 @@ static void listen_keyboard_handler(void)
     close(attr_fd);
 }
 
-static void run_kernel_mode(void)
-{
-    if (!status_check())
-        exit(1);
-
-    raw_mode_enable();
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-
-    char display_buf[DRAWBUFFER_SIZE];
-
-    fd_set readset;
-    int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
-    int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
-    read_attr = true;
-    end_attr = false;
-
-    while (!end_attr) {
-        FD_ZERO(&readset);
-        FD_SET(STDIN_FILENO, &readset);
-        FD_SET(device_fd, &readset);
-
-        int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
-        if (result < 0) {
-            printf("Error with select system call\n");
-            exit(1);
-        }
-
-        if (FD_ISSET(STDIN_FILENO, &readset)) {
-            FD_CLR(STDIN_FILENO, &readset);
-            listen_keyboard_handler();
-        } else if (read_attr && FD_ISSET(device_fd, &readset)) {
-            FD_CLR(device_fd, &readset);
-            printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
-            read(device_fd, display_buf, DRAWBUFFER_SIZE);
-            display_buf[DRAWBUFFER_SIZE - 1] = '\0';
-            printf("%s", display_buf);
-        }
-    }
-
-    raw_mode_disable();
-    fcntl(STDIN_FILENO, F_SETFL, flags);
-
-    close(device_fd);
-}
-
-static char draw_buffer[DRAWBUFFER_SIZE];
-static char turn;
-static int finish;
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
-static char table[N_GRIDS];
-
 static int draw_board(char *table)
 {
     int i = 0, k = 0;
@@ -161,6 +111,57 @@ static int draw_board(char *table)
 
     return 0;
 }
+
+static void run_kernel_mode(void)
+{
+    if (!status_check())
+        exit(1);
+
+    raw_mode_enable();
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+
+    char table_buf[DRAWBUFFER_SIZE];
+
+    fd_set readset;
+    int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
+    int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
+    read_attr = true;
+    end_attr = false;
+
+    while (!end_attr) {
+        FD_ZERO(&readset);
+        FD_SET(STDIN_FILENO, &readset);
+        FD_SET(device_fd, &readset);
+
+        int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
+        if (result < 0) {
+            printf("Error with select system call\n");
+            exit(1);
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &readset)) {
+            FD_CLR(STDIN_FILENO, &readset);
+            listen_keyboard_handler();
+        } else if (read_attr && FD_ISSET(device_fd, &readset)) {
+            FD_CLR(device_fd, &readset);
+            printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
+            read(device_fd, table_buf, N_GRIDS);
+            draw_board(table_buf);
+            printf("%s", draw_buffer);
+        }
+    }
+
+    raw_mode_disable();
+    fcntl(STDIN_FILENO, F_SETFL, flags);
+
+    close(device_fd);
+}
+
+static char turn;
+static int finish;
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+static char table[N_GRIDS];
 
 static void check_win_work_func(void *arg)
 {
